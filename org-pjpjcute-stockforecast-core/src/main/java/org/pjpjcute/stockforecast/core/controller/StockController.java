7e01a96558477d76b488504b863468d0cd4b298f
lei.zhou <lei.zhou2@mobilityasia.com.cn>
2024-04-24 18:11:08 +0800

    add user scope and stock archtect

---
+++ org-pjpjcute-stockforecast-core/src/main/java/org/pjpjcute/stockforecast/core/controller/StockController.java
@@ -0,0 +1,9 @@
+package org.pjpjcute.stockforecast.core.controller;
+
+/**
+ * @author zhoulei
+ * @create 2024/4/24
+ * @description:
+ */
+public class StockController {
+}
--- org-pjpjcute-stockforecast-core/src/main/java/org/pjpjcute/stockforecast/core/domain/stock/Stock.java
+++ org-pjpjcute-stockforecast-core/src/main/java/org/pjpjcute/stockforecast/core/domain/stock/Stock.java
@@ -1,22 +1,33 @@
 package org.pjpjcute.stockforecast.core.domain.stock;
 
+import org.pjpjcute.stockforecast.core.domain.common.MarketType;
+
+import lombok.Getter;
+
 /**
  * @author zhoulei
  * @create 2024/4/24
  * @description:
  */
+
+@Getter
 public class Stock {
     /**
      * 股票代码，作为主键
      */
-    private String symbol;
+    private String     symbol;
     /**
      * 股票名称
      */
-    private String name;
+    private String     name;
 
     /**
      * 最新价格
      */
-    private double currentPrice;
+    private double     currentPrice;
+
+    /**
+     * 股票市场
+     */
+    private MarketType marketType;
 }
